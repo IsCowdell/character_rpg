b93918ae44badd820e545dfa7d3ec38f2bb2fc7a
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-04-02 09:51:28 -0600

    trying

---
+++ trying.py
@@ -0,0 +1,23 @@
+
+from faker import Faker
+import json
+from random import randint
+
+fake = Faker()
+n1 = 5
+students = {}
+
+for i in range(n1):
+    students[i] = {
+        'id': randint(1, 10),
+        'name': fake.name(),
+        'address': fake.address(),
+        'latitude': float(fake.latitude()),
+        'longitude': float(fake.longitude())
+    }
+
+print(json.dumps(students, indent=4))
+with open('students.json', 'w') as fp:
+    json.dump(students, fp, indent=4)
+
+print(f"JSON file created with {n1} student records.")
