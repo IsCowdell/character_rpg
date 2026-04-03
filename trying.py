c6f64268335414f89e1d29a7ed5ccafb8912680f
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-04-03 09:49:53 -0600

    Testing panda out

--- trying.py
+++ trying.py
@@ -1,24 +1,7 @@
+import pandas as pd
 
-from faker import Faker
-import json
-from random import randint
 
-fake = Faker()
-n1 = 5
-students = {}
+df = pd.read_csv("orders.csv")
+print(df)
 
-for i in range(n1):
-    students[i] = {
-        'id': randint(1, 10),
-        'name': fake.name(),
-        'address': fake.address(),
-        'latitude': float(fake.latitude()),
-        'longitude': float(fake.longitude())
-    }
-
-print(json.dumps(students, indent=4))
-with open('students.json', 'w') as fp:
-    json.dump(students, fp, indent=4)
-
-print(f"JSON file created with {n1} student records.")
-
+ 
